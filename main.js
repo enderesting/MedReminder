59bdbf21ce61d9bb69cfa2997de0c6ef0586cf17
fc58165 <fc58165@alunos.fc.ul.pt>
2023-05-05 09:21:41 +0100

    added real clock

--- main.js
+++ main.js
@@ -185,3 +185,22 @@ function hide() {
             monthsIndex == 4 &&
             yearIndex == 2023)
 }
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    // s = checkTime(s);
+    document.getElementById('realtime').innerHTML = h + ":" + m;
+    // console.log(meep);
+    // var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+setInterval(startTime,10);
